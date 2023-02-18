1bc6367d814c754d0172e895f38fa5e7cce3cea5
k-shivan-reddy <f2013362h@alumni.bits-pilani.ac.in>
2023-02-18 17:26:57 +0530

    basic controller

---
+++ src/main/java/com/scaler/java_spring_basics/RestControllerClass.java
@@ -0,0 +1,14 @@
+package com.scaler.java_spring_basics;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestControllerClass {
+
+    @GetMapping("/hello")
+    public String sayHello()
+    {
+        return "Hello World";
+    }
+}
---
+++ src/test/java/com/scaler/java_spring_basics/JavaSpringBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.java_spring_basics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
